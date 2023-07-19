ddcb742ebc37df6bb37608e91574a7831d8ba868
jonese42 <135815740+jonese42@users.noreply.github.com>
2023-07-19 15:41:01 +0100

    Generating components (#1)
    * added home
    
    * added shopping list
    
    * added recipes
    
    * added upload image
    
    * added sign in
    
    * added ingredients
    
    * add method
    
    * added menu

---
+++ front-end/src/app/add-method/add-method.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-method',
+  templateUrl: './add-method.component.html',
+  styleUrls: ['./add-method.component.css']
+})
+export class AddMethodComponent {
+
+}
