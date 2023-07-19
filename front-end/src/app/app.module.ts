04f0646fad2f38192149d5d9845357f7131d0a56
Evie Jones <jonese147@cardiff.ac.uk>
2023-07-19 15:35:39 +0100

    added upload image

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { UploadImageComponent } from './upload-image/upload-image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ShoppingListComponent,
-    RecipesComponent
+    RecipesComponent,
+    UploadImageComponent
   ],
   imports: [
     BrowserModule,
