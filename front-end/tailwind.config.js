2f735a3636921bd19a3481e0e04225b5bb1e1a6d
Adam Tagg <tagga@cardiff.ac.uk>
2023-07-19 15:52:01 +0100

    Global fonts and colours and site background (starting on header)

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    fontFamily: {
+      body: ['"Montserrat"', 'sans-serif'],
+    },
+
+    colors: {
+      'ui-green': '#A1F0CA',
+      'ui-blue': '#82ADFF',
+      'ui-coral': '#FF9D7F',
+      'ui-white': '#FFFFF9',
+    },
+
     extend: {},
   },
   plugins: [],
