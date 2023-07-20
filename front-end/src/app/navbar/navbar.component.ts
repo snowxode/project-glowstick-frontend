90638a88c9711b438920b5c9b6e5290252eb8e9b
Adam Tagg <tagga@cardiff.ac.uk>
2023-07-20 16:53:07 +0100

    Coloured active nav links

--- front-end/src/app/navbar/navbar.component.ts
+++ front-end/src/app/navbar/navbar.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  styles : [`a.active { color: #82ADFF;} a.active + svg path {fill: #82ADFF;}`]
 })
 export class NavbarComponent {
 
