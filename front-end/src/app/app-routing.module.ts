56cca6cae62b9e7e4a6625f0b0a7593fc66075a4
jonese42 <135815740+jonese42@users.noreply.github.com>
2023-07-31 13:08:36 +0100

    Sort body
    * added buttons to most main pages
    
    * this better be working i will cry (#6)
    
    * sorted the duplication issue
    
    * fix home button + add filler content

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { UploadImageComponent } from './upload-image/upload-image.component';
+// import { HomeComponent } from './home/home.component';
+// import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+// import { RecipesComponent } from './recipes/recipes.component';
+// import { UploadImageComponent } from './upload-image/upload-image.component';
 import { SignInComponent } from './sign-in/sign-in.component';
-import { AddIngredientsComponent } from './add-ingredients/add-ingredients.component';
-import { AddMethodComponent } from './add-method/add-method.component';
-import { MenuComponent } from './menu/menu.component';
-import { AppComponent } from './app.component';
+// import { AddIngredientsComponent } from './add-ingredients/add-ingredients.component';
+// import { AddMethodComponent } from './add-method/add-method.component';
+// import { MenuComponent } from './menu/menu.component';
+// import { AppComponent } from './app.component';
+
 import { TestComponent } from './test/test.component';
 
 const routes: Routes = [
@@ -21,6 +22,7 @@ const routes: Routes = [
   { path: 'add-ingredients', component: TestComponent },
   { path: 'add-method', component: TestComponent },
   { path: 'menu', component: TestComponent },
+  { path: '', component: TestComponent },
 ];
 
 @NgModule({
