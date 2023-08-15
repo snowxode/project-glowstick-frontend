fa48b1b92d9aba9441f7fb87cf14ab7738097ad8
Evie Jones <jonese147@cardiff.ac.uk>
2023-08-15 19:03:25 +0100

    added privacy policy

--- front-end/src/app/footer/footer.component.ts
+++ front-end/src/app/footer/footer.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  styles : [`a.active { color: #82ADFF;} a.active + svg path {fill: #82ADFF;}`]
 })
 export class FooterComponent {
 
