317d8f28b070880365b10bf46911cfc6a1c2f558
Adam Tagg <tagga@cardiff.ac.uk>
2023-08-24 12:07:41 +0100

    Fixes

--- front-end/src/app/recipes/recipes.component.ts
+++ front-end/src/app/recipes/recipes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipes',
@@ -14,24 +15,26 @@ export class RecipesComponent implements OnInit{
     { Name: 'Cream cheese'},
     { Name: 'Strawberries'},
     { Name: 'Vanilla Extract' },
-    { Name: 'Vanilla Extract' },
-    { Name: 'Vanilla Extract' },
-    { Name: 'Vanilla Extract' },
-    { Name: 'Vanilla Extract' },
-    { Name: 'Vanilla Extract' },
-    { Name: 'Vanilla Extract' },
-    { Name: 'Vanilla Extract' },
-    { Name: 'Vanilla Extract' },
-    { Name: 'Vanilla Extract' },
-  ];
+    { Name: 'Double Cream' },];
+
   _object = Object;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
+  // TODO: make a service for this to get all of the back-end routes needed for the recipes page
   ngOnInit() {
-    this.http.get<any>('http://localhost:3000/recipes').subscribe(res => {
-      console.log(res.message);
-    });
+    this.http.get<any>('http://localhost:3000/recipes').subscribe(
+      res => {
+        console.log(res.message);
+      },
+      err => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status === 401) {
+            this.router.navigate(['/sign-in']);
+          }
+        }
+      }
+    );
   }
 }
 
