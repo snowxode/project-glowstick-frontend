02aeff9713b4693e6a9577d9d01ee39943780dd4
Adam Tagg <tagga@cardiff.ac.uk>
2023-09-12 19:47:57 +0100

    Fixed the routing problems

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,22 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TestComponent } from './test/test.component';
 import { AuthGuard } from './auth.guard';
+import { HomeComponent } from './home/home.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { TsAndCsComponent } from './ts-and-cs/ts-and-cs.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { MenuComponent } from './menu/menu.component';
+import { AddMethodComponent } from './add-method/add-method.component';
+import { AddIngredientsComponent } from './add-ingredients/add-ingredients.component';
+import { UploadImageComponent } from './upload-image/upload-image.component';
 
 const routes: Routes = [
-  { path: 'home', component: TestComponent },
-  { path: 'shopping-list', component: TestComponent, canActivate: [AuthGuard] },
-  { path: 'recipes', component: TestComponent, canActivate: [AuthGuard] },
-  { path: 'recipes/new-recipe/upload', component: TestComponent, canActivate: [AuthGuard] },
-  { path: 'sign-in', component: TestComponent },
-  { path: 'recipes/new-recipe/add-ingredients', component: TestComponent, canActivate: [AuthGuard] },
-  { path: 'recipes/new-recipe/add-method', component: TestComponent, canActivate: [AuthGuard] },
-  { path: 'menu', component: TestComponent },
-  { path: '', component: TestComponent },
-  { path: 'sign-up', component: TestComponent },
-  { path: 'privacy-policy', component: TestComponent },
-  { path: 'terms-and-conditions', component: TestComponent },
-  { path: 'contact-us', component: TestComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'shopping-list', component: ShoppingListComponent, canActivate: [AuthGuard] },
+  { path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard] },
+  { path: 'recipes/new-recipe/upload', component: UploadImageComponent, canActivate: [AuthGuard] },
+  { path: 'recipes/new-recipe/add-ingredients', component: AddIngredientsComponent, canActivate: [AuthGuard] },
+  { path: 'recipes/new-recipe/add-method', component: AddMethodComponent, canActivate: [AuthGuard] },
+  { path: 'menu', component: MenuComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
+  { path: 'terms-and-conditions', component: TsAndCsComponent },
+  { path: 'contact-us', component: ContactUsComponent },
 ];
 
 @NgModule({
